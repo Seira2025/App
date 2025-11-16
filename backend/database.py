241cf9742d94108ef379adeb4a4681e5ef73a3a5
Seira2025 <owner.seira@gmail.com>
2025-11-16 21:13:46 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, scoped_session, sessionmaker
 
-from . import config
+import config
 
 engine = create_engine(
     config.SQLALCHEMY_DATABASE_URI,
